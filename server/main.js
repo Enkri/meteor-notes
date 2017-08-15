03449c7ac40ede7ff5e5a7322666985daf06f326
Chen Lin <leoncheung1991@gmail.com>
2017-08-14 22:50:25 -0400

    Initial commit

---
+++ server/main.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
+import '../imports/api/users.js';
+import '../imports/startup/simple-schema-configuration.js';
+import moment from 'moment';
+
+Meteor.startup(() => {
+
+});
